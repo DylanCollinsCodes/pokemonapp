d9b752541945e9a61503130fa275c4ab73ade4e7
Dylan Collins <biodcollins@gmail.com>
2023-02-25 16:27:31 -0500

    style updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,20 +3,24 @@ import '../style/about.css'
 const About = () => {
   return (
     <div id='container'>
-      <h1 id="aboutH1">
-        Pokemon comparisons is a web based app to help you find better pokemon match ups.
-      </h1>
-      <ul id="aboutUl">
-        <span id="aboutSpan">Features that are on their way:</span>
-        <li>Login/Sign up</li>
-        <li>Saving pokemon teams - 🗹</li>
-        <li>Editing pokemon teams - 🗹</li>
-        <li>Recommend what pokemon to use against opponents pokemon</li>
-        <li>Display a list of typeweaknesses for your opponents pokemon</li>
-        <span id="secondSpan">Latest fixes:</span>
-        <li>Fixed an issue that could cause the page to crash</li>
-        <li>Fixed an issue on smaller screen sizes</li>
-      </ul>
+      <div id='putItAllHere'>
+        <div class='backGroundSetup'>
+          <h1 id="aboutH1">Here's you can find the most recent changes!</h1>
+        </div>
+        <div class='backGroundSetup'>
+          <ul id="aboutUl">
+            <span id="aboutSpan">Features that are on their way:</span>
+            <li>Login/Sign up</li>
+            <li>Saving pokemon teams - 🗹</li>
+            <li>Editing pokemon teams - 🗹</li>
+            <li>Recommend what pokemon to use against opponents pokemon</li>
+            <li>Display a list of typeweaknesses for your opponents pokemon</li>
+            <span id="secondSpan">Latest fixes:</span>
+            <li>Fixed an issue that could cause the page to crash</li>
+            <li>Fixed an issue on smaller screen sizes</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,20 @@ import '../style/index.css'
 const Home = () => {
   return (
     <div>
-      <h1 id="indexH1">Welcome!</h1>
-      <p id='indexP'>This website is under developement and there are new features on the way. Feel free to make any  requests and know that there are new features planned.</p>
+      <div id='indexHome'>
+        <h1 id="indexH1"class='indexHome centerMe'>Welcome to this Pokemon App!</h1>
+        <p id='indexP' class='indexHome bigTextSmall'>Welcome to the Pokemon App that lets you build teams and compare pokemon! On here you'll find the <a href="pokemon">comparison tool</a> and the <a href="teambuilder">teambuilder tool</a>. I hope you enjoy your stay at the site and I hope you come back soon to check out future changes!</p>
+        <div id='flexThisDiv'>
+          <div class="shrinkThisDiv margin-right">
+          <p class='indexHome centerMe fullWidth customHeaderHeight bigTextSmall'>Pokemon Compare Tool Guide</p>
+          <p class='indexHome fullWidth smallerText'>Hey there! I'm glad that you're here. Here's a couple tips for this tool! You can display a number of pokemon and then click on them to test their strengths and weaknesses against each other. If you insert a number this will override your list of pokemon so be careful if you don't want it reset! Lists will be savable in the future. You can however add specific pokemon to a list generated providing a number!</p>
+          </div>
+          <div class="shrinkThisDiv margin-left">
+          <p class='indexHome centerMe fullWidth customHeaderHeight bigTextSmall'>Team Builder Guide</p>
+          <p class='indexHome fullWidth smallerText'>Here are some tips to help you use the Team Builder! You can generate a team by typing in pokemon names and then save that team for future use! If you clear your cache you will your data so be careful! This will be moved to a database in the future for those who sign up! You can click on a pokemon in your active team to delete them from it and you can resave a team to update it to a new pokemon list! Enjoy your stay!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
